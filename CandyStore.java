7dbf1baeb3095259459669c1cf3b9f72e0106051
Marky2222 <124633839+Marky2222@users.noreply.github.com>
2023-02-28 15:19:28 -0500

    hi

--- CandyStore.java
+++ CandyStore.java
@@ -1,4 +1,5 @@
 public class CandyStore {
+    import java.util.Scanner;
     public static void main(String[] args) {
         // Main method. 
         
@@ -6,13 +7,17 @@ public class CandyStore {
 
     public static String candyChoices() {
         // this method should print out all the candy choices and prompt the user to make a choice
-        System.out.print("Here are the candy options: a) Reese's pieces: $2.50/0.5 kg, b) Skittles: $1.75/1 kg");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Here are the candy options:" + "a) Reese's pieces: $2.50/0.5 kg" + "b) Skittles: $1.75/1 kg" + "c) Jubjubes $0.50/1 kg" + "d) Lollipops 1 for $0.50 or 5 for $2.00" + "e) Smarties $1.50/1 kg");
+        System.out.print("Which one do you want?");
+        String choice = input.nextLine();
+        System.out.print("How much do you want?");
+        Double quantity = input.nextDouble();
 
-
-        return ""; //change this return statement to return the user's choice
+        return choice + quantity; //change this return statement to return the user's choice
     }
 
-    public static double calculateCost(String choice, int qty) {
+    public static Double calculateCost(String choice, int qty) {
         // this method should calculate the total cost of purchasing a type of candy
         return 0.0;
     }
