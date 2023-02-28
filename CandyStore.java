fda68a72a35fa12e5aeaa40d5641854c0c8f5e3f
Marky2222 <124633839+Marky2222@users.noreply.github.com>
2023-02-28 14:48:51 -0500

    hi

--- CandyStore.java
+++ CandyStore.java
@@ -1,11 +1,14 @@
 public class CandyStore {
     public static void main(String[] args) {
-        // Main method. This method should contain most of your code.
+        // Main method. 
         
     } //end main
 
     public static String candyChoices() {
         // this method should print out all the candy choices and prompt the user to make a choice
+        System.out.print("Here are the candy options: a) Reese's pieces: $2.50/0.5 kg, b) Skittles: $1.75/1 kg");
+
+
         return ""; //change this return statement to return the user's choice
     }
 
